47957193bdb3d871d7ef0e7619c5a3dd4d6db833
Ahmed Nabeel <nabeel910@protonmail.com>
2021-03-20 13:01:22 +0200

    padding

--- blocks.def.h
+++ blocks.def.h
@@ -2,8 +2,8 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 
-	{" ",	"music-indicator",	2,	18},
-	{"📦 ",	"packagecount",	600,	16},
+	{"",	"music-indicator",	2,	18},
+	{"",	"packagecount",	600,	16},
 //	{"",	"weather",	60,	16},
 	{"🌡 ",	"cputemp",	10,	12},
 	{"⬇ ",	"dwmdown",	1,	26},
@@ -11,9 +11,13 @@ static const Block blocks[] = {
 	{"🔆 ",	"dwmbrightness",	0,	10},
 	{"",	"sound",	0,	5},
 	{"📡 ",	"wifi",	5,	17},
+	{"📅 ",	"date +%a,%d-%m",	5,	20},
 	{"🕒 ",	"date +%I:%M ",	5,	20},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
 static char delim[] = " | ";
 static unsigned int delimLen = 5;
+// Sets delimiters around the full statusbar. NULL character ('\0') means no delimeter.
+static char leftpad[]  = " ";
+static char rightpad[] = " ";
